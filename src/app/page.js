8e78ace7509be3feba6b04a48e62f6ef1f172988
Sebastian <sebastian.flores@elaniin.com>
2023-06-14 22:06:44 -0600

    Initialize project :

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import LogIn from "@/app/login/components/log";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-2xl font-rubik font-bold">HI</h1>
+    </main>
+  );
+}
