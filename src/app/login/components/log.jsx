e856356071d6b3e80aa0da54d60317f7938ead8a
Sebs14 <00196919@uca.edu.sv>
2023-11-18 03:09:29 -0600

    Fix formatting and add missing semicolons

--- src/app/login/components/log.jsx
+++ src/app/login/components/log.jsx
@@ -1,16 +1,16 @@
-"use client"
+"use client";
 import React from "react";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import AuthService from "../../../services/auth.service"
+import AuthService from "../../../services/auth.service";
 import axios from "axios";
 
 const Log = () => {
   const router = useRouter();
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,25 +20,29 @@ const Log = () => {
           router.push("/historialDeViajes");
         },
         (error) => {
+          AuthService.deleteAll();
           console.log("error1".error);
         }
       );
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
     <article className="flex min-h-screen items-center justify-between">
       <div className="flex flex-col items-center justify-center w-[50%]">
-        <Image width={512} height={512} src={"/logIn.jpg"} alt="logIn"/>
+        <Image width={512} height={512} src={"/logIn.jpg"} alt="logIn" />
         <h1 className=" text-4xl font-rubik font-bold">R y S Transportes</h1>
       </div>
       <div className="flex flex-col items-start pl-40 justify-center w-[50%] bg-[#9DD9D2] min-h-screen">
         <h2 className="font-bold text-[#3E4856] font-rubik text-4xl text-left pb-12">
           Inicia sesión
         </h2>
-        <form onSubmit={handleSubmit} className="flex flex-col items-start justify-center w-[50%]">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-start justify-center w-[50%]"
+        >
           <label className=" font-medium pb-4">Correo</label>
           <input
             type={"email"}
@@ -55,7 +59,10 @@ const Log = () => {
             className=" rounded-lg py-2 px-2 w-full"
             placeholder="password"
           ></input>
-          <button type="submit"  className="mt-20 py-4 text-center w-full font-medium font-rubik text-white bg-[#FF8811] rounded-lg">
+          <button
+            type="submit"
+            className="mt-20 py-4 text-center w-full font-medium font-rubik text-white bg-[#FF8811] rounded-lg"
+          >
             Inicia sesión
           </button>
         </form>
